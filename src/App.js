2b9c923c9389567038753c12e7314581cacf861d
Joy S <stepjoyous@gmail.com>
2023-11-29 22:06:10 -0800

    Refactored categories out into its own component and simplified App.js.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,10 @@
-import CategoryItem from './components/category-item/category-item.component'
-
-import './categories.styles.scss'
+import Categories from "./components/categories/categories.component";
 
 const App = () => {
 
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
-
   return (
-    <section className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category}/>
-      ))}
+    <section className="App">
+      <Categories/>
     </section>
   );
 }
