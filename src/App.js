32c3562d88e36ef3aa5f3c59e72366076184da98
Joy S <stepjoyous@gmail.com>
2023-11-29 20:46:47 -0800

    Created temp categories data and rendered within categories-container section.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+const categories = [
+  {
+    id: 1,
+    title: "Hats",
+    description: "Shop now!"
+  },
+  {
+    id: 2,
+    title: "Sweaters",
+    description: "Sweater weather's here!"
+  },
+  {
+    id: 3,
+    title: "Jackets",
+    description: "Keep warm with our outerwear!"
+  },
+  {
+    id: 4,
+    title: "Shirts",
+    description: "Layer up with our shirts!"
+  },
+  {
+    id: 5,
+    title: "Pants",
+    description: "Keep your bottoms warm!"
+  },
+]
+
 const App = () => {
-  return <div>
-    <h1>Hello World!</h1>
-  </div>;
+
+  const renderCategoryCards = (cats) => {
+    return cats.map( cat => <div className="card-container" key={cat.id}>
+    <h1>{cat.title}</h1>
+    <p>{cat.description}</p>
+  </div>)
+  }
+
+  return (
+    <section className="categories-container">
+      {renderCategoryCards(categories)}
+    </section>
+  );
 }
 
 export default App;
