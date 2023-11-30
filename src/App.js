e0f35565d24f9830f49e704947d7e04490fe5477
Joy S <stepjoyous@gmail.com>
2023-11-29 21:45:15 -0800

    Updated categories.styles.css and categories with json data.

--- src/App.js
+++ src/App.js
@@ -1,41 +1,43 @@
 import './categories.styles.scss'
 
-const categories = [
-  {
-    id: 1,
-    title: "Hats",
-    subtitle: "Shop now!"
-  },
-  {
-    id: 2,
-    title: "Sweaters",
-    subtitle: "Sweater weather's here!"
-  },
-  {
-    id: 3,
-    title: "Jackets",
-    subtitle: "Keep warm with our outerwear!"
-  },
-  {
-    id: 4,
-    title: "Shirts",
-    subtitle: "Layer up with our shirts!"
-  },
-  {
-    id: 5,
-    title: "Pants",
-    subtitle: "Keep your bottoms warm!"
-  },
-]
-
 const App = () => {
 
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
   const renderCategoryCards = (cats) => {
-    return cats.map( ({id, title, subtitle}) => <div className="card-container" key={id}>
-    <div className="card-bg-img"></div>
+    return cats.map( ({ id, title, imageUrl }) => <div className="card-container" key={id}>
+    <div className="card-bg-img" style={{
+      backgroundImage: `url(${imageUrl})`
+    }}></div>
     <div className="category-body-container">
       <h1>{title}</h1>
-      <p>{subtitle}</p>
+      <p>Shop Now!</p>
     </div>
   </div>)
   }
